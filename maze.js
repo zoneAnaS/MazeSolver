5e9cdf19b0ae42a9a2d2b31c4798f86aaf4827d9
Your Name <shaikhanas@gmail.com>
2023-01-07 01:28:14 +0530

    Updated

--- maze.js
+++ maze.js
@@ -1,86 +1,75 @@
 let maze_container = document.getElementById("maze_container");
 let maze_size = document.getElementById("maze_size");
+let wayText=document.getElementById("ways");
 
+const five = [[[0, 1, 0, 1, 0], [0, 0, 0, 0, 0], [0, 0, 1, 0, 1], [0, 1, 0, 0, 1], [1, 0, 0, 0, 0]],
+[[0, 0, 0, 0, 0],[0, 0, 1, 1, 0],[0, 1, 0, 0, 0],[1, 0, 0, 1, 1],[0, 1, 0, 0, 0]],
+[[0, 0, 1, 0, 0],[1, 0, 1, 0, 0],[0, 0, 0, 0, 0],[0, 1, 0, 1, 0],[0, 0, 0, 1, 0]],
+[[0, 0, 1, 0, 0],[0, 0, 0, 0, 0],[1, 1, 0, 1, 0],[0, 0, 0, 0, 1],[0, 0, 1, 0, 0]],
+[[0, 0, 0, 1, 0],[0, 1, 0, 1, 0],[0, 0, 0, 0, 1],[0, 1, 1, 0, 0],[1, 0, 0, 0, 0]],
+[[0, 1, 0, 0, 0],[0, 0, 1, 1, 0],[0, 1, 0, 0, 0],[0, 0, 0, 1, 0],[0, 1, 0, 1, 0]],
+[[0, 0, 0, 0, 0],[0, 1, 0, 1, 0],[1, 0, 1, 0, 0],[0, 1, 0, 0, 1],[0, 0, 1, 0, 0]],
+[[0, 0, 0, 0, 0],[1, 1, 1, 1, 0],[0, 0, 0, 0, 0],[0, 1, 1, 1, 1],[0, 0, 0, 0, 0]],
+[[0, 0, 1, 0, 0],[0, 0, 0, 1, 0],[1, 1, 0, 0, 1],[0, 1, 1, 0, 0],[0, 0, 1, 1, 0]],
+[[0, 0, 1, 0, 0],[1, 0, 0, 0, 0],[0, 0, 1, 0, 1],[0, 1, 0, 0, 0],[0, 0, 0, 0, 0]]
+];
+const seven = [[[0, 1, 1, 0, 0, 0, 0], [0, 0, 0, 0, 0, 1, 0], [1, 1, 0, 0, 0, 1, 0], [0, 0, 1, 1, 1, 0, 0], [1, 0, 0, 0, 0, 0, 0], [0, 0, 1, 1, 1, 1, 1], [1, 0, 0, 0, 0, 0, 0]],
+[[0, 0, 1, 0, 0, 0, 1],[1, 0, 0, 0, 1, 0, 0],[0, 1, 1, 1, 0, 1, 0],[0, 0, 0, 0, 0, 0, 0],[0, 1, 1, 1, 1, 1, 1],[0, 0, 0, 1, 0, 0, 0],[0, 1, 0, 0, 0, 1, 0]],
+[[0, 0, 0, 1, 1, 1, 1],[0, 0, 0, 0, 0, 0, 0],[1, 1, 1, 1, 0, 0, 1],[1, 0, 0, 0, 0, 0, 1],[1, 0, 0, 1, 1, 1, 1],[1, 0, 0, 0, 0, 0, 0],[1, 1, 1, 1, 1, 0, 0]],
+[[0, 0, 0, 1, 1, 1, 1],[0, 0, 0, 0, 0, 1, 0],[1, 1, 1, 0, 0, 0, 1],[1, 0, 0, 0, 1, 1, 1],[1, 0, 1, 1, 0, 0, 0],[1, 0, 0, 0, 0, 1, 0],[1, 1, 1, 1, 1, 0, 0]],
+[[0, 0, 0, 1, 1, 1, 1],[0, 0, 0, 0, 0, 0, 0],[1, 0, 0, 1, 1, 1, 0],[1, 0, 0, 1, 0, 0, 0],[1, 0, 1, 0, 0, 1, 1],[1, 1, 0, 0, 1, 0, 0],[1, 1, 1, 0, 0, 0, 0]],
+[[0, 0, 0, 1, 0, 0, 0],[1, 0, 1, 0, 0, 1, 0],[0, 0, 1, 1, 0, 1, 0],[0, 1, 0, 0, 0, 1, 0],[0, 0, 0, 1, 1, 0, 0],[0, 0, 1, 0, 0, 0, 1],[1, 0, 0, 1, 1, 0, 0]],
+[[0, 0, 0, 0, 0, 0, 0],[0, 1, 1, 0, 1, 1, 0],[0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 1, 0, 0, 0],[0, 1, 0, 0, 0, 1, 0],[0, 0, 1, 1, 1, 0, 0],[0, 0, 0, 0, 0, 0, 0]],
+[[0, 0, 1, 0, 0, 0, 0],[1, 0, 1, 0, 1, 0, 1],[1, 0, 1, 0, 1, 0, 0],[1, 0, 1, 0, 1, 0, 1],[1, 0, 1, 0, 1, 0, 0],[1, 0, 1, 0, 1, 0, 1],[1, 0, 0, 0, 1, 0, 0]],
+[[0, 0, 1, 0, 0, 0, 0],[1, 0, 0, 0, 1, 0, 1],[0, 1, 1, 1, 0, 0, 0],[1, 0, 0, 0, 0, 0, 1],[0, 0, 0, 0, 1, 0, 1],[0, 1, 0, 1, 0, 1, 0],[0, 0, 0, 0, 0, 0, 0]],
+[[0, 0, 0, 1, 0, 0, 0],[1, 1, 0, 0, 0, 1, 0],[0, 0, 1, 1, 1, 0, 0],[0, 0, 0, 0, 0, 0, 1],[0, 1, 1, 1, 1, 1, 0],[0, 1, 0, 0, 0, 1, 0],[0, 0, 0, 1, 0, 0, 0]]];
+const nine = [[0, 0, 1, 0, 0, 0, 0, 0, 0], [1, 0, 1, 0, 0, 0, 0, 0, 0], [0, 0, 1, 0, 0, 1, 0, 0, 0], [0, 1, 1, 0, 0, 1, 0, 0, 0], [0, 0, 0, 0, 1, 0, 0, 0, 0], [1,1,1,1,1,1,1,1, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 1, 0, 1, 1], [0, 0, 0, 0, 0, 0, 0, 0, 0]];
 
-
-let bool=false
 makeGrid(maze_size.value);
 maze_size.addEventListener("change", () => {
     let size = maze_size.value;
-    if(!bool){makeGrid(size);}
+    
+    makeGrid(size);
 
 })
 
  function makeGrid(n) {
-    const five = [[0, 1, 0, 1, 0], [0, 0, 0, 0, 0], [0, 0, 1, 0, 1], [0, 1, 0, 0, 1], [1, 0, 0, 0, 0]];
-    const seven = [[0, 1, 1, 0, 0, 0, 0], [0, 0, 0, 0, 0, 1, 0], [1, 1, 0, 0, 0, 1, 0], [0, 0, 1, 1, 1, 0, 0], [1, 0, 0, 0, 0, 0, 0], [0, 0, 1, 1, 1, 1, 1], [1, 0, 0, 0, 0, 0, 0]];
-    const nine = [[0, 0, 1, 0, 0, 0, 0, 0, 0], [1, 0, 1, 0, 0, 0, 0, 0, 0], [0, 0, 1, 0, 0, 1, 0, 0, 0], [0, 1, 1, 0, 0, 1, 0, 0, 0], [0, 0, 0, 0, 1, 0, 0, 0, 0], [1,1,1,1,1,1,1,1, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 1, 0, 1, 1], [0, 0, 0, 0, 0, 0, 0, 0, 0]];
-    let curr = five;
+    let randIndex=Math.floor(Math.random() * 10);
+    let curr = five[randIndex];
+    
     if (n == 9) {
         curr = nine;
     } else if (n == 7) {
-        curr = seven;
+        curr = seven[randIndex];
     }
+    console.log(randIndex,curr)
     //calling display function
     displaymaze(curr, n)
     //solving the current maze
     let t = solveMaze(curr, curr.length, 0, 0, "", []);
-   
+    //formatts the total ways string
+    let way=getFormattedWays(t?t.length:0);
+    // displays button (max=5)
     
-    console.log(t)
-    if (t.length) {
-        t = t[0];
-        let k = 0;
-        let i = 0;
-        let j = 0;
-        // while (k < t.length) {
-            
-        //     let pathdiv = document.querySelector(`.cell:nth-child(${(i * curr.length) + j + 1})`)
-        //     console.log(pathdiv);
-        //     path(pathdiv)
-        //     if (t[k] == "D") {
-        //         i++;
-        //     } else if (t[k] == "U") {
-        //         i--;
-        //     } else if (t[k] == "L") {
-        //         j--;
-        //     } else if (t[k] == "R") {
-        //         j++;
-        //     }
-        //     k++
-
-        // }
-        //RUDLRD
-        let intt= setInterval(()=>{
-            
-            if(k==t.length){
-                let cell = document.querySelector(`.cell:nth-child(${n * n})`)
-        cell.classList.add("green");
-                bool=false;
-                clearInterval(intt);
-                
-            }else{
-                bool=true;
-            }
-            
-            let cell = document.querySelector(`.cell:nth-child(${(i * curr.length) + j + 1})`)
-            path(cell)
-            if (t[k] == "D") {
-                i++;
-            } else if (t[k] == "U") {
-                i--;
-            } else if (t[k] == "L") {
-                j--;
-            } else if (t[k] == "R") {
-                j++;
-            }
-            k++
-        },200)
+    displaySolutionButtons(t?t.length:0)
+    
+   wayText.innerHTML=way;
+    if (t) {
+        
+        let ans = t[0];
+        displaySolution(ans,curr)
+        let buttons=document.querySelectorAll(".sbtn");
+        buttons.forEach((item)=>{
+            item.addEventListener("click",function(){
+                displaymaze(curr, n)
+                displaySolution(t[this.dataset.id-1],curr)
+            })
+        })
         
     }else{
        console.log("No paths found")
     }
-
+    
 
 
 }
@@ -92,6 +81,7 @@ maze_size.addEventListener("change", () => {
 }
 //display maze function
 function displaymaze(curr, n) {
+
     maze_container.innerHTML = "";
     maze_container.style = `grid-template-columns:repeat(${n},1fr)`
     let str = "";
@@ -105,6 +95,8 @@ function displaymaze(curr, n) {
 
     }
     maze_container.innerHTML = str;
+    
+    
 }
 //maze solving function
 function solveMaze(arr, n, i, j, string, ans) {
@@ -126,13 +118,79 @@ function solveMaze(arr, n, i, j, string, ans) {
     solveMaze(arr, n, i + 1, j, string + "D", ans);
     solveMaze(arr, n, i - 1, j, string + "U", ans);
     solveMaze(arr, n, i, j - 1, string + "L", ans);
+    arr[i][j] = 0;
     return ans
+}
+//formatted ways
+function getFormattedWays(Nways){
+    Nways+="";
+    let str="";
+    let c=3;
+    let l=0;
+    if(Nways.length>3){
+        for(let i=Nways.length-1;i>=0;i--){
+            l++;
+            if(l==c){
+                str+=Nways[i]+","
+                c=2
+                l=0
+            }else{
+                str+=Nways[i]
+            }
+        }
+        return str.split("").reverse().join("")
+    }else{
+        return Nways;
+    }
+}
+function displaySolution(t,curr,){
+    let n=curr.length
+    let k = 0;
+            let i = 0;
+            let j = 0;
+        while (k < t.length) {
+            
+            let pathdiv = document.querySelector(`.cell:nth-child(${(i * curr.length) + j + 1})`)
+            setTimeout(()=>{
+                path(pathdiv)
+            },(k+1)*200)
+            
+            if (t[k] == "D") {
+                i++;
+            } else if (t[k] == "U") {
+                i--;
+            } else if (t[k] == "L") {
+                j--;
+            } else if (t[k] == "R") {
+                j++;
+            }
+            k++
+
+        }
+        let pathdiv = document.querySelector(`.cell:nth-child(${n*n})`)
+            
+        setTimeout(()=>{
+            path(pathdiv)
+        },(k+1)*200)
+        
+}
+function displaySolutionButtons(N){
+    let buttonContainer=document.getElementById("buttons")
+    buttonContainer.innerHTML="";
+    // <button class="sbtn">1</button>
+    N=N>5?5:N;
+    let i=1;
+    while(i<=N){
+        
+        buttonContainer.innerHTML+=`<button class="sbtn" data-id=${i}>${i}</button>`
+        
+        i++;
+    }
+    
 }
 
 
-
-
-
+// setInterval(()=>{console.log(},500)
 
 
 
